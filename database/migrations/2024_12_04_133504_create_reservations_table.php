34a7f9e092965ea21b782692cbe3e325734a7919
Cansyell <haiguyswehhh@gmail.com>
2024-12-04 22:05:42 +0700

    commit

---
+++ database/migrations/2024_12_04_133504_create_reservations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->string('customer_phone');
+            $table->string('customer_email');
+            $table->dateTime('reservation_date');
+            $table->enum('status', [
+                'pending', 
+                'confirmed', 
+                'in_progress', 
+                'completed', 
+                'cancelled'
+            ])->default('pending');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
